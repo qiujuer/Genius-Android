94c16ffe26a9549daa5f4a7ddb9c78faa8fc6972
Qiujuer <qiujuer@live.cn>
2014-09-24 10:34:05 +0800

    Add ToolUtils

--- library/src/main/java/net/qiujuer/genius/util/ToolUtils.java
+++ library/src/main/java/net/qiujuer/genius/util/ToolUtils.java
@@ -1,15 +1,28 @@
 package net.qiujuer.genius.util;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.telephony.TelephonyManager;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.List;
 
 /**
  * Created by QiuJu
  * on 2014/8/13.
  */
 public final class ToolUtils {
+    /**
+     * Sleep time
+     * Don't throw an InterruptedException exception
+     *
+     * @param time long time
+     */
     public static void sleepIgnoreInterrupt(long time) {
         try {
             Thread.sleep(time);
@@ -18,6 +31,13 @@ public final class ToolUtils {
         }
     }
 
+    /**
+     * Copy file to file
+     *
+     * @param source Source File
+     * @param target Target File
+     * @return isCopy ok
+     */
     public static boolean copyFile(File source, File target) {
         boolean bFlag = false;
         FileInputStream in = null;
@@ -53,5 +73,67 @@ public final class ToolUtils {
             }
         }
         return bFlag;
+    }
+
+    /**
+     * Equipment is started for the first time the generated number
+     * Are potential "9774d56d682e549c"
+     *
+     * @param context Context
+     * @return Number
+     */
+    public static String getAndroidId(Context context) {
+        return android.provider.Settings.System.getString(context.getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+    }
+
+    /**
+     * This device's SN
+     *
+     * @return SerialNumber
+     */
+    public static String getSerialNumber() {
+        String serialNumber = android.os.Build.SERIAL;
+        if ((serialNumber == null || serialNumber.length() == 0 || serialNumber.contains("unknown"))) {
+            String[] keys = new String[]{"ro.boot.serialno", "ro.serialno"};
+            for (String key : keys) {
+                try {
+                    Method systemProperties_get = Class.forName("android.os.SystemProperties").getMethod("get", String.class);
+                    serialNumber = (String) systemProperties_get.invoke(null, key);
+                    if (serialNumber != null && serialNumber.length() > 0 && !serialNumber.contains("unknown"))
+                        break;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return serialNumber;
+    }
+
+    /**
+     * Get TelephonyManager DeviceId
+     * This Need READ_PHONE_STATE permission
+     *
+     * @param context Context
+     * @return DeviceId
+     */
+    public static String getDeviceId(Context context) {
+        return ((TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId();
+    }
+
+    /**
+     * The system contains specified App packageName
+     *
+     * @param context     Context
+     * @param packageName App packageName
+     * @return isAvailable
+     */
+    private boolean isAvailablePackage(Context context, String packageName) {
+        final PackageManager packageManager = context.getPackageManager();
+        List<PackageInfo> infoList = packageManager.getInstalledPackages(0);
+        for (PackageInfo info : infoList) {
+            if (info.packageName.equalsIgnoreCase(packageName))
+                return true;
+        }
+        return false;
     }
 }
