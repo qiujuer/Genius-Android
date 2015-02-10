f1f2e36d3f5c1e268e6be5fb9b8f23d5827363eb
Qiujuer <qiujuer@live.cn>
2015-02-10 16:34:01 +0800

    Add English Notes

--- sample/src/main/java/net/qiujuer/genius/sample/CaseActivity.java
+++ sample/src/main/java/net/qiujuer/genius/sample/CaseActivity.java
@@ -31,17 +31,20 @@ public class CaseActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case);
 
-        // 初始化
+        // Init to use
+        // 初始化使用
         Genius.initialize(getApplication());
 
         mText = (TextView) findViewById(R.id.text);
 
-        // 添加回调
+        // Add callback
+        // 添加回调显示
         Log.addCallbackListener(new Log.LogCallbackListener() {
             @Override
             public void onLogArrived(final Log data) {
                 try {
-                    //异步显示到界面
+                    // Async to show
+                    // 异步显示到界面
                     UIKit.runOnMainThreadAsync(new Runnable() {
                         @Override
                         public void run() {
@@ -55,7 +58,7 @@ public class CaseActivity extends ActionBarActivity {
             }
         });
 
-        // 开始测试
+        // Start
         testLog();
         testTool();
         testToolKit();
@@ -69,27 +72,30 @@ public class CaseActivity extends ActionBarActivity {
     protected void onDestroy() {
         super.onDestroy();
         mText = null;
-        // 销毁
+        // Dispose when you don't use
         Genius.dispose();
     }
 
 
     /**
-     * 测试 工具包
+     * Test Tool
      */
     private void testToolKit() {
-        // 同步模式一般用于更新界面同时等待界面更新完成后才能继续往下走的情况
-        // 异步模式一般用于线程操作完成后统一更新主界面的情况
+        // Synchronous mode in the main thread when operating the child thread will enter the waiting,
+        // until the main thread processing is completed
+        // 同步模式在主线程操作时子线程将进入等待，直到主线程处理完成
+
+        // Asynchronous mode the child thread parallel operation with the main thread, don't depend on each other
+        // 异步模式子线程与主线程并行操作，不相互依赖等待
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                // 测试运行在主线程中的情况，所以建立子线程进行调用
-                // 方法中的操作将会切换到主线程中执行
                 String msg = "ToolKit:";
                 long start = System.currentTimeMillis();
+
+                // Test synchronization mode,
+                // in this mode method first to execute commands on the queue, waiting for the main thread
                 // 测试同步模式，在该模式下
-                // 子线程将会等待其执行完成
-                // 在主线程中调用该方法后才能继续往下走
                 // 该方法首先会将要执行的命令放到队列中，等待主线程执行
                 UIKit.runOnMainThreadSync(new Runnable() {
                     @Override
@@ -97,18 +103,21 @@ public class CaseActivity extends ActionBarActivity {
                         Tools.sleepIgnoreInterrupt(20);
                     }
                 });
-                msg += "同步时间:" + (System.currentTimeMillis() - start) + ", ";
+                msg += "Sync Time:" + (System.currentTimeMillis() - start) + ", ";
 
                 start = System.currentTimeMillis();
+
+                // Test asynchronous mode,
+                // in this mode the child thread calls the method added to the queue, can continue to go down, will not be blocked
                 // 测试异步模式，在该模式下
-                // 子线程调用该方法后既可继续往下走，并不会阻塞
+                // 子线程调用该方法加入到队列后，可继续往下走，并不会阻塞
                 UIKit.runOnMainThreadAsync(new Runnable() {
                     @Override
                     public void run() {
                         Tools.sleepIgnoreInterrupt(20);
                     }
                 });
-                msg += "异步时间:" + (System.currentTimeMillis() - start) + " ";
+                msg += "Async Time:" + (System.currentTimeMillis() - start) + " ";
                 Log.v(TAG, msg);
             }
         });
@@ -121,81 +130,97 @@ public class CaseActivity extends ActionBarActivity {
     }
 
     /**
-     * 日志测试
+     * Log
      */
     private void testLog() {
-        //是否调用系统Android Log，发布时可设置为false
+        // Whether to call system Android Log, release can be set to false
+        // 是否调用系统Android Log，发布时可设置为false
         Log.setCallLog(true);
 
-        //清理存储的文件
+        // Clear the storage file
+        // 清理存储的文件
         Log.clearLogFile();
 
-        //是否开启写入文件，存储最大文件数量，单个文件大小（Mb）
+        // Whether open is written to the file, stored maximum number of files, a single file size (Mb)
+        // 是否开启写入文件，存储最大文件数量，单个文件大小（Mb）
         Log.setSaveLog(true, 10, 1);
 
-        //设置是否监听外部存储插入操作
-        //开启时插入外部设备（SD）时将拷贝存储的日志文件到外部存储设备
-        //此操作依赖于是否开启写入文件功能，未开启则此方法无效
-        //是否开启，SD卡目录
+        // Set whether to monitor external storage inserts
+        // 设置是否监听外部存储插入操作
+        // Open when inserting an external device (SD) to store the log file copy to external storage devices
+        // 开启时插入外部设备（SD）时将拷贝存储的日志文件到外部存储设备
+        // This operation depends on whether written to the file open function, the method is invalid when not open
+        // 此操作依赖于是否开启写入文件功能，未开启则此方法无效
+        // Parameters: whether open, SD card catalog
+        // 参数：是否开启，SD卡目录
         Log.setCopyExternalStorage(true, "Test/Logs");
 
-        //设置日志等级
-        //VERBOSE为5到ERROR为1依次递减
+        // Set show log level
+        // 设置显示日志等级
+        // VERBOSE: 5  ERROR:1, decline in turn
+        // VERBOSE为5到ERROR为1依次递减
         Log.setLevel(Log.ALL);
 
-        Log.v(TAG, "测试日志 VERBOSE 级别。");
-        Log.d(TAG, "测试日志 DEBUG 级别。");
-        Log.i(TAG, "测试日志 INFO 级别。");
-        Log.w(TAG, "测试日志 WARN 级别。");
-        Log.e(TAG, "测试日志 ERROR 级别。");
+        Log.v(TAG, "LOG VERBOSE Level");
+        Log.d(TAG, "LOG DEBUG Level");
+        Log.i(TAG, "LOG INFO Level");
+        Log.w(TAG, "LOG WARN Level");
+        Log.e(TAG, "LOG ERROR Level");
 
         Log.setLevel(Log.INFO);
-        Log.v(TAG, "二次测试日志 VERBOSE 级别。");
-        Log.d(TAG, "二次测试日志 DEBUG 级别。");
-        Log.i(TAG, "二次测试日志 INFO 级别。");
-        Log.w(TAG, "二次测试日志 WARN 级别。");
-        Log.e(TAG, "二次测试日志 ERROR 级别。");
+        Log.v(TAG, "LOG VERBOSE Level");
+        Log.d(TAG, "LOG DEBUG Level");
+        Log.i(TAG, "LOG INFO Level");
+        Log.w(TAG, "LOG WARN Level");
+        Log.e(TAG, "LOG ERROR Level");
 
         Log.setLevel(Log.ALL);
     }
 
     /**
-     * 测试MD5
+     * MD5
      */
     private void testHashUtils() {
-        Log.i(TAG, "HashUtils：QIUJUER的MD5值为：" + HashUtils.getMD5String("QIUJUER"));
-        //文件MD5不做演示，传入file类即可
+        Log.i(TAG, "HashUtils：QIUJUER's MD5：" + HashUtils.getMD5String("QIUJUER"));
+        //HashUtils.getMD5String(new File("FilePath"));
     }
 
     /**
-     * 测试固定长度队列
+     * FixedList
+     * 固定长度队列
      */
     private void testFixedList() {
-        //初始化最大长度为5
+        // Init max length 5
+        // 初始化最大长度为5
         FixedList<Integer> list = new FixedList<Integer>(5);
         Log.i(TAG, "FixedList:" + list.size() + " ," + list.getMaxSize());
-        //添加4个元素
+        // Add
+        // 添加4个元素
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         Log.i(TAG, "FixedList:" + list.size() + " ," + list.getMaxSize());
-        //继续追加2个
+
+        // 继续追加2个
         list.add(5);
         list.add(6);
         Log.i(TAG, "FixedList:" + list.size() + " ," + list.getMaxSize());
-        //调整最大长度
+        // Changed MaxSize
+        // 调整最大长度
         list.setMaxSize(6);
         list.add(7);
         Log.i(TAG, "FixedList:" + list.size() + " ," + list.getMaxSize());
         list.add(8);
         Log.i(TAG, "FixedList:" + list.size() + " ," + list.getMaxSize());
-        //缩小长度，自动删除前面多余部分
+        // Narrow length, automatically deletes the spare parts
+        // 缩小长度，自动删除前面多余部分
         list.setMaxSize(3);
         Log.i(TAG, "FixedList:" + list.size() + " ," + list.getMaxSize());
         list.add(9);
         Log.i(TAG, "FixedList:" + list.size() + " ," + list.getMaxSize());
-        //添加一个列表进去，自动删除多余部分
+        // Add a list, automatically remove unwanted parts
+        // 添加一个列表进去，自动删除多余部分
         List<Integer> addList = new ArrayList<Integer>();
         addList.add(10);
         addList.add(11);
@@ -203,29 +228,32 @@ public class CaseActivity extends ActionBarActivity {
         addList.add(13);
         list.addAll(addList);
         Log.i(TAG, "FixedList:AddList:" + list.toString() + " " + list.size() + " ," + list.getMaxSize());
-        //采用poll方式弹出元素
+        // Using poll pop-up elements
+        // 采用poll方式弹出元素
         Log.i(TAG, "FixedList:Poll:[" + list.poll() + "] " + list.size() + " ," + list.getMaxSize());
         Log.i(TAG, "FixedList:Poll:[" + list.poll() + "] " + list.size() + " ," + list.getMaxSize());
         Log.i(TAG, "FixedList:Poll:[" + list.poll() + "] " + list.size() + " ," + list.getMaxSize());
-        //末尾插入元素与add一样
+        // Add to end insert
+        // 末尾插入元素与add一样
         list.addLast(14);
         list.addLast(15);
         list.addLast(16);
         list.addLast(17);
         list.addLast(18);
         Log.i(TAG, "FixedList:AddLast:" + list.toString() + " " + list.size() + " ," + list.getMaxSize());
-        //从头部插入，默认删除尾部超出部分
+        // From the head insert, delete the default tail beyond part
+        // 从头部插入，默认删除尾部超出部分
         list.addFirst(19);
         list.addFirst(20);
         Log.i(TAG, "FixedList:AddFirst:" + list.toString() + " " + list.size() + " ," + list.getMaxSize());
-        //Remove与poll类似不过不返回删除元素，只会删除一个
-        list.remove();
-        Log.i(TAG, "FixedList:Remove:" + list.toString() + " " + list.size() + " ," + list.getMaxSize());
-        //清空操作
+
+        // Clear
+        // 清空操作
         list.clear();
         Log.i(TAG, "FixedList:Clear:" + list.toString() + " " + list.size() + " ," + list.getMaxSize());
 
-        //使用List操作,最大长度2
+        // List
+        // 使用List操作,最大长度2
         List<Integer> list1 = new FixedList<Integer>(2);
         list1.add(1);
         list1.add(2);
@@ -239,77 +267,89 @@ public class CaseActivity extends ActionBarActivity {
     }
 
     /**
+     * Command
      * 测试命令行执行
      */
     private void testCommand() {
-        //同步
+        // Sync
+        // 同步
         Thread thread = new Thread() {
             public void run() {
-                //调用方式与ProcessBuilder传参方式一样
+                // The same way call way and the ProcessBuilder mass participation
+                // 调用方式与ProcessBuilder传参方式一样
                 Command command = new Command(Command.TIMEOUT, "/system/bin/ping",
                         "-c", "4", "-s", "100",
                         "www.baidu.com");
-                //同步方式执行
+
                 String res = Command.command(command);
-                Log.i(TAG, "\n\nCommand 同步：" + res);
+                Log.i(TAG, "\n\nCommand Sync：" + res);
             }
         };
         thread.setDaemon(true);
         thread.start();
 
-        //异步
+        // Async
+        // 异步
         Command command = new Command("/system/bin/ping",
                 "-c", "4", "-s", "100",
                 "www.baidu.com");
 
-        //异步方式执行
-        //采用回调方式，无需自己建立线程
-        //传入回调后自动采用此种方式
+        // Asynchronous execution using callback methods,
+        // do not need to build a thread
+        // callback by listener
+        // 异步方式执行
+        // 采用回调方式，无需自己建立线程
+        // 传入回调后自动采用此种方式
         Command.command(command, new Command.CommandListener() {
             @Override
             public void onCompleted(String str) {
-                Log.i(TAG, "\n\nCommand 异步 onCompleted：\n" + str);
+                Log.i(TAG, "\n\nCommand Async onCompleted：\n" + str);
             }
 
             @Override
             public void onCancel() {
-                Log.i(TAG, "\n\nCommand 异步 onCancel");
+                Log.i(TAG, "\n\nCommand Async onCancel");
             }
 
             @Override
             public void onError(Exception e) {
-                Log.i(TAG, "\n\nCommand 异步 onError:" + (e != null ? e.toString() : "null"));
+                Log.i(TAG, "\n\nCommand Async onError:" + (e != null ? e.toString() : "null"));
             }
         });
     }
 
 
     /**
+     * NetTool
      * 基本网络功能测试
      */
     public void testNetTool() {
-        //所有目标都可为IP地址
         Thread thread = new Thread() {
             public void run() {
-                //包数，包大小，目标，是否解析IP
+                // Packets， Packet size，The target，Whether parsing IP
+                // 包数，包大小，目标，是否解析IP
                 Ping ping = new Ping(4, 32, "www.baidu.com", true);
                 ping.start();
                 Log.i(TAG, "Ping：" + ping.toString());
-                //目标，可指定解析服务器
+                // target
+                // 目标，可指定解析服务器
                 DnsResolve dns = null;
                 try {
-                    // 添加深圳电信DNS服务器地址进行解析
+                    // Add DNS service
+                    // 添加DNS服务器
                     dns = new DnsResolve("www.baidu.com", InetAddress.getByName("202.96.128.166"));
                     dns.start();
                     Log.i(TAG, "DnsResolve：" + dns.toString());
                 } catch (UnknownHostException e) {
                     e.printStackTrace();
                 }
-                //目标，端口
+                // target port
+                // 目标，端口
                 Telnet telnet = new Telnet("www.baidu.com", 80);
                 telnet.start();
                 Log.i(TAG, "Telnet：" + telnet.toString());
-                //目标
+                // target
+                // 目标
                 TraceRoute traceRoute = new TraceRoute("www.baidu.com");
                 traceRoute.start();
                 Log.i(TAG, "\n\nTraceRoute：" + traceRoute.toString());
