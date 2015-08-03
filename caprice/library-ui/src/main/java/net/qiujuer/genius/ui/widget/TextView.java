4a370b5a649a4ca79050ea2ed5321f3e67c39926
Qiujuer <qiujuer@live.cn>
2015-08-03 21:57:28 +0800

    Improve TextView , Add BorderAttrs

--- caprice/library-ui/src/main/java/net/qiujuer/genius/ui/widget/TextView.java
+++ caprice/library-ui/src/main/java/net/qiujuer/genius/ui/widget/TextView.java
@@ -22,55 +22,185 @@ package net.qiujuer.genius.ui.widget;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.os.Build;
 import android.util.AttributeSet;
 
+import net.qiujuer.genius.res.Resource;
+import net.qiujuer.genius.ui.GeniusUi;
 import net.qiujuer.genius.ui.R;
-import net.qiujuer.genius.ui.widget.attribute.FontAttribute;
+import net.qiujuer.genius.ui.drawable.shape.BorderShape;
 
 /**
  * TextView this is quickly set up
  */
 public class TextView extends android.widget.TextView {
-    private FontAttribute mFontAttribute;
+    public final static int BORDER_LEFT = 0x0001;
+    public final static int BORDER_RIGHT = 0x0010;
+    public final static int BORDER_TOP = 0x0100;
+    public final static int BORDER_BOTTOM = 0x1000;
+    public final static int BORDER_ALL = BORDER_LEFT | BORDER_RIGHT | BORDER_TOP | BORDER_BOTTOM;
+    private int mBorder;
+    private int mBorderColor;
+    private int mBorderSize;
+    private Drawable mBorderDrawable;
+    private boolean isAttachedToWindow;
 
     public TextView(Context context) {
-        this(context, null);
+        super(context);
     }
 
     public TextView(Context context, AttributeSet attrs) {
-        //com.android.internal.R.attr.textViewStyle
-        this(context, attrs, 0);
+        super(context, attrs);
+        init(attrs, 0, 0);
     }
 
     public TextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs, defStyleAttr, 0);
+        init(attrs, defStyleAttr, 0);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public TextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context, attrs, defStyleAttr, defStyleRes);
+        init(attrs, defStyleAttr, defStyleRes);
     }
 
-    private void init(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        if (mFontAttribute == null)
-            mFontAttribute = new FontAttribute();
+    private void init(AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        if (attrs == null)
+            return;
 
-        if (attrs != null) {
+        final Context context = getContext();
+        final float density = getResources().getDisplayMetrics().density;
 
-            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.TextView);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TextView);
+        int border = a.getInt(R.styleable.TextView_gBorder, -1);
+        int borderSize = a.getDimensionPixelOffset(R.styleable.TextView_gBorderSize, (int) density);
+        int borderColor = a.getColor(R.styleable.TextView_gBorderColor, Resource.Color.GREY);
+        String fontFile = a.getString(R.styleable.TextView_gFont);
+        a.recycle();
 
-
-            a.recycle();
-        }
+        setBorder(border, borderSize, borderColor);
 
         // Check for IDE preview render
         if (!this.isInEditMode()) {
-            Typeface typeface = mFontAttribute.getFont(context);
+            Typeface typeface = GeniusUi.getFont(getContext(), fontFile);
             if (typeface != null) setTypeface(typeface);
         }
+    }
+
+    /**
+     * In this init BorderSharp and Drawable
+     */
+    private void setBorder(int border, int borderSize, int borderColor) {
+        mBorder = border;
+        mBorderSize = borderSize;
+        mBorderColor = borderColor;
+
+        if (mBorder == -1 || mBorder == 0) {
+            mBorderDrawable = null;
+        } else {
+            RectF borderRect;
+            if ((border & BORDER_ALL) == BORDER_ALL)
+                borderRect = new RectF(borderSize, borderSize, borderSize, borderSize);
+            else {
+                int l = 0, t = 0, r = 0, b = 0;
+
+                if ((border & BORDER_LEFT) == BORDER_LEFT)
+                    l = borderSize;
+                if ((border & BORDER_RIGHT) == BORDER_RIGHT)
+                    r = borderSize;
+                if ((border & BORDER_TOP) == BORDER_TOP)
+                    t = borderSize;
+                if ((border & BORDER_BOTTOM) == BORDER_BOTTOM)
+                    b = borderSize;
+
+                borderRect = new RectF(l, t, r, b);
+            }
+
+            if (mBorderDrawable == null) {
+                ShapeDrawable drawable = new ShapeDrawable(new BorderShape(borderRect));
+                Paint paint = drawable.getPaint();
+                paint.setColor(borderColor);
+                mBorderDrawable = drawable;
+            } else {
+                ShapeDrawable drawable = (ShapeDrawable) mBorderDrawable;
+                Paint paint = drawable.getPaint();
+                paint.setColor(borderColor);
+                BorderShape shape = (BorderShape) drawable.getShape();
+                shape.setBorder(borderRect);
+            }
+        }
+
+        if (isAttachedToWindow)
+            invalidate();
+    }
+
+    public void setBorder(int flag) {
+        if (mBorder != flag) {
+            mBorder = flag;
+            setBorder(mBorder, mBorderSize, mBorderColor);
+        }
+    }
+
+    public void setBorderSize(int size) {
+        if (mBorderSize != size) {
+            mBorderSize = size;
+            setBorder(mBorder, mBorderSize, mBorderColor);
+        }
+    }
+
+    public void setBorderColor(int color) {
+        if (mBorderColor != color) {
+            mBorderColor = color;
+            setBorder(mBorder, mBorderSize, mBorderColor);
+        }
+    }
+
+    public int getBorder() {
+        return mBorder;
+    }
+
+    public int getBorderColor() {
+        return mBorderColor;
+    }
+
+    public int getBorderSize() {
+        return mBorderSize;
+    }
+
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        isAttachedToWindow = true;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Drawable drawable = mBorderDrawable;
+        if (drawable != null) {
+            drawable.setBounds(0, 0, getWidth(), getHeight());
+        }
+    }
+
+    @Override
+    protected boolean verifyDrawable(Drawable who) {
+        Drawable drawable = mBorderDrawable;
+        return (drawable != null && who == drawable) || super.verifyDrawable(who);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable drawable = mBorderDrawable;
+        if (drawable != null && mBorderSize > 0 && mBorderColor != 0)
+            drawable.draw(canvas);
+        super.onDraw(canvas);
     }
 }
