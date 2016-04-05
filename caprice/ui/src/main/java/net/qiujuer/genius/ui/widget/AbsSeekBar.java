b7be2436e48b8aaef127d3210fd423ae87b2190a
qiujuer <qiujuer@live.cn>
2016-04-05 15:31:04 +0800

    Fixed SeekBar set max or min bug #49

--- caprice/ui/src/main/java/net/qiujuer/genius/ui/widget/AbsSeekBar.java
+++ caprice/ui/src/main/java/net/qiujuer/genius/ui/widget/AbsSeekBar.java
@@ -408,6 +408,8 @@ public abstract class AbsSeekBar extends View {
 
         if (mValue < mMin || mValue > mMax) {
             setProgress(mMin);
+        } else {
+            updateThumbPosForScale(-1);
         }
     }
 
@@ -442,6 +444,8 @@ public abstract class AbsSeekBar extends View {
 
         if (mValue < mMin || mValue > mMax) {
             setProgress(mMin);
+        } else {
+            updateThumbPosForScale(-1);
         }
     }
 
@@ -962,9 +966,9 @@ public abstract class AbsSeekBar extends View {
 
     /**
      * Interface to transform the current internal value of this AbsSeekBar to anther one for the visualization.
-     * <p/>
+     * <p>
      * This will be used on the floating bubble to display a different value if needed.
-     * <p/>
+     * <p>
      * Using this in conjunction with {@link #setIndicatorFormatter(String)} you will be able to manipulate the
      * value seen by the user
      *
