aeabc6f2c0e8d32ef5fc7e38b4333df42c379f41
Qiujuer <qiujuer@live.cn>
2016-07-11 23:22:20 +0800

    update  loading drawable license

--- caprice/ui/src/main/java/net/qiujuer/genius/ui/drawable/LoadingDrawable.java
+++ caprice/ui/src/main/java/net/qiujuer/genius/ui/drawable/LoadingDrawable.java
@@ -1,9 +1,7 @@
 /*
- * Copyright (C) 2015 Qiujuer <qiujuer@live.cn>
+ * Copyright (C) 2014-2016 Qiujuer <qiujuer@live.cn>
  * WebSite http://www.qiujuer.net
- * Created 10/16/2015
- * Changed 10/23/2015
- * Author Qiujuer
+ * Author qiujuer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
