ffe49594ed4b039ecb49e2150b5439860a857626
Qiujuer <qiujuer@live.cn>
2016-12-15 14:45:03 +0800

    Remove some unused values

--- caprice/res/src/test/java/net/qiujuer/genius/res/ExampleUnitTest.java
+++ caprice/res/src/test/java/net/qiujuer/genius/res/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package net.qiujuer.genius.res;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
